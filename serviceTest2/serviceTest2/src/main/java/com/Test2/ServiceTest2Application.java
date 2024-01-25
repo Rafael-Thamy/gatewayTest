643144c606c53da01feaac773fd44eceffd3e795
Rafael-Thamy <MNMRAFA@GMAIL.COM>
2024-01-25 18:02:01 -0300

    forstOne

---
+++ serviceTest2/serviceTest2/src/main/java/com/Test2/ServiceTest2Application.java
@@ -0,0 +1,13 @@
+package com.Test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTest2Application.class, args);
+	}
+
+}
